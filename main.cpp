f736a258396a6e5cfcf4a118a9bed4be8948facd
Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>
2020-06-12 15:18:19 +0200

    remove useless std::remove_cv_t
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@ template<typename K, typename V>
 std::map<V, K> inverted_map(const std::map<K, V> &in) {
     std::map<V, K> ret;
     auto it = std::transform(in.begin(), in.end(), std::inserter(ret, ret.begin()), [](auto &&item) {
-        return std::pair<V, std::remove_cv_t<K>>(item.second, item.first);
+        return std::pair<V, K>(item.second, item.first);
     });
     return ret;
+}
+
+template<typename T>
+void pretty_function(T &&t) {
+    std::puts(__PRETTY_FUNCTION__);
 }
 
 int main() {
@@ -16,6 +21,7 @@ int main() {
     m[48] = 84;
     m[64] = 84;
     auto res = inverted_map(m);
+    pretty_function(res);
     for(auto &&item:res) {
         std::cout << std::to_string(item.first) << ": " << std::to_string(item.second) << std::endl;
     }
